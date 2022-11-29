05a7b3efa36d4aa41e87acd6aa89c79708dae35d
Christian Traintinger <c.traintinger@gmx.net>
2022-11-29 19:50:42 +0100

    End of chapter 4

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -3,10 +3,18 @@ package com.polarbookshop.catalogservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
+
 @RestController
 public class HomeController {
+	private final PolarProperties polarProperties;
+	
+	public HomeController(PolarProperties polarProperties) {
+		this.polarProperties = polarProperties;
+	}
+
 	@GetMapping("/")
 	public String getGreeting() {
-		return "Welcome to the book catalog!";
+		return this.polarProperties.getGreeting();
 	}
 }
