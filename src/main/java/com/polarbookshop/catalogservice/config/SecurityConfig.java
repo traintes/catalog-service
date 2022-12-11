131c35fa3b603597447fb727d55ece7809629bf6
Christian Traintinger <c.traintinger@gmx.net>
2022-12-11 17:14:34 +0100

    End of chapter 13

--- src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 			.authorizeHttpRequests(authorize -> authorize
+				.mvcMatchers("/actuator/**").permitAll()
 				.mvcMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
 				.anyRequest().hasRole("employee")
 			)
--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -2,6 +2,8 @@ package com.polarbookshop.catalogservice.web;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import com.polarbookshop.catalogservice.domain.BookService;
 @RestController
 @RequestMapping("books")
 public class BookController {
+	private static final Logger log = LoggerFactory.getLogger(BookController.class);
 	private final BookService bookService;
 
 	public BookController(BookService bookService) {
@@ -27,28 +30,33 @@ public class BookController {
 	
 	@GetMapping
 	public Iterable<Book> get() {
+		log.info("Fetching the list of books in the catalog");
 		return this.bookService.viewBookList();
 	}
 	
 	@GetMapping("{isbn}")
 	public Book getByIsbn(@PathVariable String isbn) {
+		log.info("Fetching the book with ISBN {} from the catalog", isbn);
 		return this.bookService.viewBookDetails(isbn);
 	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Book post(@Valid @RequestBody Book book) {
+		log.info("Adding a new book to the catalog with ISBN {}", book.isbn());
 		return this.bookService.addBookToCatalog(book);
 	}
 	
 	@DeleteMapping("{isbn}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable String isbn) {
+		log.info("Deleting book with ISBN {}", isbn);
 		this.bookService.removeBookFromCatalog(isbn);
 	}
 	
 	@PutMapping("{isbn}")
 	public Book put(@PathVariable String isbn, @Valid @RequestBody Book book) {
+		log.info("Updating book with ISBN {}", isbn);
 		return this.bookService.editBookDetails(isbn, book);
 	}
 }
